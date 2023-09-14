c243043826a02608fa2000949e2eabb772db4a83
Daniel Oaks <daniel@danieloaks.net>
2023-09-14 11:07:00 +1000

    Get random names

--- bh/data.go
+++ bh/data.go
@@ -3,9 +3,31 @@
 
 package bh
 
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+)
+
+//go:embed moby-word-lists/names-f.txt
+var namesF string
+
+//go:embed moby-word-lists/names-m.txt
+var namesM string
+
+func getNameList() (names []string) {
+	names = strings.Split(namesF, "\n")
+	names = append(names, strings.Split(namesM, "\n")...)
+
+	return names
+}
+
 type Data struct {
 }
 
 func GenerateData(config BirdhousesConfig) (data Data) {
+	names := getNameList()
+	fmt.Println(names)
+
 	return data
 }
